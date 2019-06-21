d833bc8fa85fe80fd2c69806865e712d709425e1
Daniil <anatoliikljukin@mail.ru>
2019-06-21 23:45:55 +0400

    style TaskWorker

--- TasksLibrary/TasksLibrary/TaskWorker.cs
+++ TasksLibrary/TasksLibrary/TaskWorker.cs
@@ -3,6 +3,6 @@
     public class TaskWorker
     {
         public void BubbleSort(int[,] array, ISortable sortMethod, Direction d)
-            => sortMethod.Order(array, d);
+            => sortMethod.Sort(array, d);
     }
 }
